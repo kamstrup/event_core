8d97dbe187c0680cfc6e739437e8087456781eeb
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@xamarin.com>
2015-03-19 11:11:39 +0100

    More unit tests for idle handlers

--- test/test_idle.rb
+++ test/test_idle.rb
@@ -29,4 +29,35 @@ class TestIdle < Test::Unit::TestCase
       assert_equal false, idle.closed?
     }
   end
+
+  def test_2idle
+    did_run1 = false
+    did_run2 = false
+
+    idle1 = @loop.add_idle { did_run1 = true }
+    idle2 = @loop.add_idle { did_run2 = true }
+
+    @loop.step
+
+    assert_equal true, did_run1
+    assert_equal true, did_run2
+    assert_equal false, idle1.closed?
+    assert_equal false, idle2.closed?
+  end
+
+  def test_nidle_nsteps
+    nsteps = []
+    idles = []
+    (1..10).each {|i|
+      nsteps << 0
+      idles << @loop.add_idle { nsteps[i-1] += 1 }
+    }
+
+    (1..7).each {  @loop.step }
+
+    (1..10).each {|i|
+      assert_equal 7, nsteps[i-1]
+      assert_equal false, idles[i-1].closed?
+    }
+  end
 end
